09b76a98f3e9678625efa30e520588b1785ef783
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-07-28 22:42:09 -0400

    updates to the site.  may drop the images

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,9 @@
+{/* <div data-_id="5b5d086223b9cc0014548050" class="card">
+<div class="card-header">
+  <h3>
+    <a class="article-link" target="_blank" rel="noopener noreferrer" href="https://www.nytimes.com/2018/07/26/realestate/a-comfy-fit-in-williamsburg.html">A Comfy Fit in Williamsburg</a>
+    <a class="btn btn-success save">Save Article</a>
+  </h3>
+</div>
+<div class="card-body">Unable to afford a suitable place near his Manhattan school, a fashion student discovered that stretching his budget just a bit opened new doors.</div>
+</div> */}
