d8047090e52df29aabd768880a63a2f5e9c3d6b7
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-08-01 09:41:22 -0400

    added X to notes

--- public/app.js
+++ public/app.js
@@ -77,6 +77,46 @@ function buildSavedArticleCards(){
  // ********************** End Build Saved Articles HTML for Scrape NY Times ********************** \\
 
 
+ // ************************************************* Build Notes Modal for Article ************************************************************ \\
+function buildNotesModal(data,mongo_id){
+    var html = `
+    <div class="bootbox modal fade show" data-mongoid="${mongo_id}" tabindex="-1" role="dialog" style="display: block;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="bootbox-close-button close" data-dismiss="modal" aria-hidden="true" style="margin-top: -10px;">×</button>
+                    <div class="bootbox-body">
+                        <div class="container-fluid text-center">
+    
+                            <h4>Notes For Article: ${data.title}</h4>
+                            <hr>
+                            <ul class="list-group note-container">`;
+    
+                            if(data.note.length > 0){
+                                for(var i = 0; i < data.note.length; i++){
+                                    html += `
+                                    <li class="list-group-item note">${data.note[i]}<button class="btn btn-danger note-delete">x</button></li>`
+                                }
+                            } else{
+                                html += `
+                                <li class="list-group-item">No notes for this article yet.</li>`;
+                            }
+    
+                            html += 
+                            `</ul>
+                            <textarea placeholder="New Note" rows="4" cols="60"></textarea>
+                            <button class="btn btn-success save">Save Note</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
+    return html
+}
+// ******************************************************************************************************************************************** \\
+
+
  // *********************************** POST save articles to MongoDB ********************************* \\ 
 $(document).on('click', '#saveArticle', function(){
     var inputArticle; 
@@ -154,15 +194,6 @@ $(document).on('click', 'a.btn.btn-info.notes', function(){
             console.error(e);
         }
     })
-    // }). 
-    // then(function(data) {
-    //     console.log('hello');
-    //     console.log(data);
-    // });  
-
-    // var html = testHTML();
-    // $("body").append(html);
-    
 });
 // ************************************************************************************************************* \\ 
 
@@ -195,41 +226,4 @@ $(document).on('click', 'button.bootbox-close-button.close', function(){
 
 
 
-// ************************************************* Build Nots Modal for Article ************************************************************ \\
-function buildNotesModal(data,mongo_id){
-    var html = `
-    <div class="bootbox modal fade show" data-mongoid="${mongo_id}" tabindex="-1" role="dialog" style="display: block;">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <button type="button" class="bootbox-close-button close" data-dismiss="modal" aria-hidden="true" style="margin-top: -10px;">×</button>
-                    <div class="bootbox-body">
-                        <div class="container-fluid text-center">
-    
-                            <h4>Notes For Article: ${data.title}</h4>
-                            <hr>
-                            <ul class="list-group note-container">`;
-    
-                            if(data.note.length > 0){
-                                for(var i = 0; i < data.note.length; i++){
-                                    html += `
-                                    <li class="list-group-item">${data.note[i]}.</li>`
-                                }
-                            } else{
-                                html += `
-                                <li class="list-group-item">No notes for this article yet.</li>`;
-                            }
-    
-                            html += 
-                            `</ul>
-                            <textarea placeholder="New Note" rows="4" cols="60"></textarea>
-                            <button class="btn btn-success save">Save Note</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>`
-    return html
-}
-// ******************************************************************************************************************************************** \\
+
